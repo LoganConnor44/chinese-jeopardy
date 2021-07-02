2258fc77f8f257656cf4977e299f139bf9b3f58c
Logan Connor <logan.connor@floridadep.gov>
2021-07-02 15:53:28 -0400

    updating existing components to include a sidebar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -5,6 +6,7 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import AppDrawer from './AppDrawer';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -20,16 +22,28 @@ const useStyles = makeStyles((theme: Theme) =>
     }),
 );
 
+
+
 const ButtonAppBar = () => {
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+    const toggleDrawer = () => {
+        setIsDrawerOpen(!isDrawerOpen);
+    };
+
     const classes = useStyles();
 
     return (
         <div className={classes.root}>
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                    <IconButton edge="start" 
+                        className={classes.menuButton} 
+                        color="inherit" 
+                        aria-label="menu"
+                        onClick={toggleDrawer}>
                         <MenuIcon />
                     </IconButton>
+                    <AppDrawer isOpen={isDrawerOpen} toggleDrawer={toggleDrawer} />
                     <Typography variant="h6" className={classes.title}>
                         Chinese Jeopardy
                     </Typography>
